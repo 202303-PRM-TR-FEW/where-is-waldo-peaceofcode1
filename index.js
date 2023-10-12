05c3afec96a55b4241d8a93d63e42c15d8c85df6
jafar bino <binocoder@gmail.com>
2023-10-12 18:50:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
+  ["B", "B", "B"],
+  ["B", "B", "B"],
+  ["B", "J", "B"]
+];
+
+const jafar = [
+["c", "c", "c", "c"],
+["c", "c", "c", "J"]
+];
+
+const bino = [
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "J", "O"],
+  ["O", "O", "O", "O"]
+];
+
 
 const whereIsWaldo = (matrix) => {
-  
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === "J") {
+        return [i, j];
+      }
+    }
+  }
 }
+
+console.log(whereIsWaldo(example));
+console.log(whereIsWaldo(jafar));
+console.log(whereIsWaldo(bino));
